d6a4350d027a3c9cb706ab0a6c9e0ecf7e5f3d5e
FernandoSC18 <f-ercho44otmail.com>
2022-03-11 02:55:26 -0600

    Add polyfills.js
    With some first usefull imports

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
+ 
+import './polyfills' 
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css'; 
+import reportWebVitals from './reportWebVitals'; 
+ 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const element = <>
+    <h1>Hello, world</h1>
+
+    <p>Test to safari 9 / webkit 601.1 </p>
+
+</>;
+
+ReactDOM.render(element, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
